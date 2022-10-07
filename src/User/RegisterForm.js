4143c43074539b471fd79e7cb2b23b614536c027
201724554 <pay7845@naver.com>
2022-10-07 16:13:54 +0900

    .

--- src/User/RegisterForm.js
+++ src/User/RegisterForm.js
@@ -16,11 +16,11 @@ function RegisterForm()
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div>
-                    <input placeholder="아이디" {...register("username", {required: {value: true, message: "아이디를 입력하세요"}})}/>
+                    <input placeholder="아이디: 5~20자" {...register("username", {required: {value: true, message: "아이디를 입력하세요"}})}/>
                     {errors.username && <span style={{color:'red', fontSize:"13px"}}>{errors.username.message}</span>}
                 </div>
                 <div>
-                    <input placeholder="비밀번호" type="password" {...register("password", {required: {value: true, message: "비밀번호를 입력하세요"}})}/>
+                    <input placeholder="비밀번호: 8~20자" type="password" {...register("password", {required: {value: true, message: "비밀번호를 입력하세요"}})}/>
                     {errors.password && <span style={{color:'red', fontSize:"13px"}}>{errors.password.message}</span>}
                 </div>
                 <div>
@@ -28,7 +28,7 @@ function RegisterForm()
                     {errors.email && <span style={{color:'red', fontSize:"13px"}}>{errors.email.message}</span>}
                 </div>
                 <div>
-                    <input placeholder="기기 MAC 주소" {...register("userDeviceMAC", {required: {value: true, message: "MAC 주소를 입력하세요"}})}/>
+                    <input placeholder="기기 MAC 주소 XX-XX-XX-XX" {...register("userDeviceMAC", {required: {value: true, message: "MAC 주소를 입력하세요"}})}/>
                     {errors.userDeviceMAC && <span style={{color:'red', fontSize:"13px"}}>{errors.userDeviceMAC.message}</span>}
                 </div>
                 <input type="submit"/>

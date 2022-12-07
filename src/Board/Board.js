93304205047e0902de8e87b857e33ab8f5ff8a6f
unknown <als6898@pusan.ac,kr>
2022-12-07 22:48:01 +0900

    12/7일 연구실

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -12,7 +12,11 @@ function Board(){
     <h1>Board</h1>
 
   </div>
+ 
 </section>
+<div class="input-group">
+    <input type="text" class="form-control" placeholder="Search " aria-label="Input group example" aria-describedby="btnGroupAddon"/>
+  </div>
 <ul class="nav nav-tabs">
   <li class="nav-item">
     <a class="nav-link active" aria-current="page" href="#">Active</a>
@@ -23,10 +27,8 @@ function Board(){
   <li class="nav-item">
     <a class="nav-link" href="#">Link</a>
   </li>
-  <li class="nav-item">
-    <a class="nav-link disabled">Disabled</a>
-  </li>
 </ul>
+
 <div className="album py-5 bg-light">
   <div className="container">
 
@@ -39,7 +41,7 @@ function Board(){
             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <div className="d-flex justify-content-between align-items-center">
               <div className="btn-group">
-                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                
                 
               </div>
               
@@ -56,7 +58,7 @@ function Board(){
             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <div className="d-flex justify-content-between align-items-center">
               <div className="btn-group">
-                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                
                 
               </div>
               <small className="text-muted">올린 날짜</small>
@@ -72,56 +74,7 @@ function Board(){
             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <div className="d-flex justify-content-between align-items-center">
               <div className="btn-group">
-                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
                 
-              </div>
-              <small className="text-muted">올린 날짜</small>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="col">
-        <div className="card shadow-sm">
-          <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-          <div className="card-body">
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="btn-group">
-                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                
-              </div>
-              <small className="text-muted">올린 날짜</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col">
-        <div className="card shadow-sm">
-          <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-          <div className="card-body">
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="btn-group">
-                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                
-              </div>
-              <small className="text-muted">올린 날짜</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col">
-        <div className="card shadow-sm">
-          <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-          <div className="card-body">
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="btn-group">
-                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
                 
               </div>
               <small className="text-muted">올린 날짜</small>
@@ -138,7 +91,7 @@ function Board(){
             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <div className="d-flex justify-content-between align-items-center">
               <div className="btn-group">
-                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                
                 
               </div>
               <small className="text-muted">올린 날짜</small>
@@ -154,7 +107,7 @@ function Board(){
             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <div className="d-flex justify-content-between align-items-center">
               <div className="btn-group">
-                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                
                 
               </div>
               <small className="text-muted">올린 날짜</small>
@@ -170,7 +123,56 @@ function Board(){
             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <div className="d-flex justify-content-between align-items-center">
               <div className="btn-group">
-                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                
+                
+              </div>
+              <small className="text-muted">올린 날짜</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="col">
+        <div className="card shadow-sm">
+          <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+          <div className="card-body">
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="btn-group">
+                
+                
+              </div>
+              <small className="text-muted">올린 날짜</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card shadow-sm">
+          <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+          <div className="card-body">
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="btn-group">
+                
+                
+              </div>
+              <small className="text-muted">올린 날짜</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card shadow-sm">
+          <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+          <div className="card-body">
+            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="btn-group">
+                
                 
               </div>
               <small className="text-muted">올린 날짜</small>
@@ -181,16 +183,17 @@ function Board(){
     </div>
   </div>
 </div>
-<div className="d-flex justify-content-between align-items-center">
 
-<button type="button" class="btn btn-secondary" id="button_set">◀</button>
-    <button type="button" class="btn btn-secondary" id="button_set">1</button>
-    <button type="button" class="btn btn-secondary" id="button_set">2</button>
-    <button type="button" class="btn btn-secondary" id="button_set">3</button>
-    <button type="button" class="btn btn-secondary" id="button_set">4</button>
-    <button type="button" class="btn btn-secondary" id="button_set">5</button>
-    <button type="button" class="btn btn-secondary" id="button_set">6</button>
-    <button type="button" class="btn btn-secondary" id="button_set">▶</button>
+<div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+  <div class="btn-group me-2" role="group" aria-label="First group">
+    <button type="button" class="btn btn-outline-secondary">◀</button>
+    <button type="button" class="btn btn-outline-secondary">1</button>
+    <button type="button" class="btn btn-outline-secondary">2</button>
+    <button type="button" class="btn btn-outline-secondary">3</button>
+    <button type="button" class="btn btn-outline-secondary">4</button>
+    <button type="button" class="btn btn-outline-secondary">▶</button>
+  </div>
+  
 </div>
 
 </main>

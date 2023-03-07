b40ecee050191b10d60ef3ec8fa9e85daf0d4e78
201724554 <pay7845@naver.com>
2023-03-08 04:10:52 +0900

    fix: 서버 ip 수정

--- src/Socket/SocketConnect.js
+++ src/Socket/SocketConnect.js
@@ -31,7 +31,7 @@ function SocketConnect(props) {
     }, 5000);
 
     function register() {
-        const sock = new SockJS("http://13.124.30.108:8080/client/socket");
+        const sock = new SockJS("http://13.124.156.11:8080/client/socket");
         stompClient = stomp.over(sock);
         stompClient.connect({authorization: localStorage.getItem("refresh")}, onConnected, onError)
     }
